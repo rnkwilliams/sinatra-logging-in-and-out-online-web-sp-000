b141bf39fca2ec0f2f42bfe9582daa7fcd34abc0
Ranika Williams <rnkprice07@gmail.com>
2019-09-18 23:18:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,9 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username], password: params[:password])
     if @user
       session[:user_id] = @user.id
-      redirect '/account'
+      redirect to '/account'
+    else
+      render 
   end
 
   get '/account' do
